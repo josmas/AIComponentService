037059b070d1d0d94d567806158a8895a144a5c8
josmas <josmasflores@gmail.com>
2013-07-31 11:42:04 -0400

    Launch the Service from an AlarmManager
      Changes a one time launch for the Service to an AlarmManager that will launch
      the service every 30 seconds.

--- AIComponentService/src/main/java/org/jos/aicomponentservice/SettingsActivity.java
+++ AIComponentService/src/main/java/org/jos/aicomponentservice/SettingsActivity.java
@@ -1,9 +1,14 @@
 package org.jos.aicomponentservice;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+
+import java.util.Calendar;
 
 public class SettingsActivity extends Activity {
 
@@ -13,9 +18,15 @@ public class SettingsActivity extends Activity {
     setContentView(R.layout.activity_settings);
 
     //Creating an intent to start ComponentService - This is an IntentService that will
-    //shut itself down automatically.
+    //shut itself down automatically, and it will be created every 30 seconds by an AlarmManager.
+    Calendar cal = Calendar.getInstance();
+
     Intent intent = new Intent(this, ComponentService.class);
-    startService(intent);
+    PendingIntent pIntent = PendingIntent.getService(this, 0, intent, 0);
+
+    AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+    // Start every 30 seconds
+    alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pIntent);
   }
 
   @Override
